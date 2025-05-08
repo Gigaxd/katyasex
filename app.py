b6042383cdbdc816f5d14e6e072c9e8c2cb248b3
Gigaxd <gribo332@gmail.com>
2025-05-09 00:55:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import os  # импортируем os для доступа к переменным окружения
 
 app = Flask(__name__)
 
@@ -21,4 +22,6 @@ def random_phrase():
     return random.choice(phrases)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Получаем порт из переменной окружения, если он не задан, используем 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
