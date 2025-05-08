f900d03b2feb5383eaeb8bab06517d46a57f4a0b
Gigaxd <gribo332@gmail.com>
2025-05-09 00:40:58 +0300

    Add files via upload

---
+++ nuda/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+phrases = [
+    "Скинь фоточки с бутиками",
+    "Покажи попочку",
+    "Дай засунуть носик в пупочек",
+    "Сядь на личико",
+    "Срочно нужно лизнуть подмышечку",
+    "Отправь поцелуйчик в голосовом"
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/random")
+def random_phrase():
+    return random.choice(phrases)
+
+if __name__ == "__main__":
+    app.run(debug=True)
